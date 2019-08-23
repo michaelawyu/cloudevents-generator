94fb7dd33aef6e17d862b547e7d84572c9b1e3b8
michaelawyu <chenyumic@google.com>
2019-08-23 03:01:04 -0700

    Added bin/ and script for compilation.

---
+++ build.sh
@@ -0,0 +1,6 @@
+GOOS=darwin GOARCH=amd64 cd src/ && go build . && mv src ../bin/cloud-events-generator-darwin-amd64 && cd ..
+GOOS=darwin GOARCH=386 cd src/ && go build . && mv src ../bin/cloud-events-generator-darwin-386 && cd ..
+GOOS=linux GOARCH=amd64 cd src/ && go build . && mv src ../bin/cloud-events-generator-linux-amd64 && cd ..
+GOOS=linux GOARCH=386 cd src/ && go build . && mv src ../bin/cloud-events-generator-linux-386 && cd ..
+GOOS=windows GOARCH=amd64 cd src/ && go build . && mv src ../bin/cloud-events-generator-windows-amd64.exe && cd ..
+GOOS=windows GOARCH=386 cd src/ && go build . && mv src ../bin/cloud-events-generator-windows-386.exe && cd ..
